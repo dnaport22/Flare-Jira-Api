f8f7dc60e7c12f11d93ce74659d443d10e73e56a
deep-nav <navdeep.dhuti@cameronandwilding.com>
2017-05-22 17:37:14 +0100

    Parsing time

--- api/service.py
+++ api/service.py
@@ -11,8 +11,10 @@ class Service():
   PROJECT_ALIAS = 'TP'
 
   def log_time(self, data):
-    url = Service.BASE_URL + '/issue/' + self.parse_ticket_number(data['issue']) + '/worklog'
-    body = json.dumps({"comment": data['comment'], "started": data['date'], "timeSpentSeconds": data['timeSpent']})
+    ticket_number = self.parse_ticket_number(data['issue'])
+    time_worked = self.parse_time(int(data['timeSpent']))
+    url = Service.BASE_URL + '/issue/' + ticket_number + '/worklog'
+    body = json.dumps({"comment": data['comment'], "started": data['date'], "timeSpentSeconds": time_worked})
     header = {"Content-Type": "application/json"}
     re = requests.post(url, data=body, headers=header, auth=HTTPBasicAuth(self.get_user_email(), self.get_user_pass()))
 
@@ -35,6 +37,12 @@ class Service():
   def parse_ticket_number(self, issue):
     number = re.search(r'\d+', issue).group()
     return Service.PROJECT_ALIAS + '-' + number
+
+  def parse_time(self, time):
+    if time < 8:
+      return time * 3600
+    else:
+      return time * 60
 
 
   def test(self, data):
